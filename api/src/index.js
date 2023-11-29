c6f7d20a28e38138796f2d6a74d0500cb4016d1f
Shanthinath Shedabale <shanthinathshedabale17@gmail.com>
2023-11-29 22:04:15 +0530

    create middleware  and a function to handle possible errors

--- api/src/index.js
+++ api/src/index.js
@@ -23,3 +23,15 @@ connectDB()
 
 //app.use("/api/user",userRouter);
 app.use("/api/auth",authRouter);
+
+//error respons 
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+ 
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
