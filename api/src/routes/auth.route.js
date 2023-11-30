0ef003a0bd28ce04b75c31601910b2efa3201bfa
Shanthinath Shedabale <shanthinathshedabale17@gmail.com>
2023-11-30 19:16:09 +0530

    create sign in api route

--- api/src/routes/auth.route.js
+++ api/src/routes/auth.route.js
@@ -1,9 +1,10 @@
-import { signup } from "../controller/auth.controller.js";
+import { signin, signup } from "../controller/auth.controller.js";
 import express from "express";
 
 const router = express.Router();
 
 router.post('/signup',signup );
+router.post('/signin',signin );
 
 export default router;
 
