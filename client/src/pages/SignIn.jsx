274bf8cdcf6d3c1b7ef1877c525cdadb2e48865f
Shanthinath Shedabale <shanthinathshedabale17@gmail.com>
2023-12-01 01:09:47 +0530

    small error fix it

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -41,8 +41,20 @@ export default function SignIn() {
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
       <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
-        <input type='text' placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
-        <input type='text' placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
+      <input
+          type='email'
+          placeholder='email'
+          className='border p-3 rounded-lg'
+          id='email'
+          onChange={handleChange}
+        />
+        <input
+          type='password'
+          placeholder='password'
+          className='border p-3 rounded-lg'
+          id='password'
+          onChange={handleChange}
+        />
         <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'loading....':'sign in'}</button>
       </form>
       <div className='flex gap-2 mt-5'>
